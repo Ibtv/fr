4114bae92aa9de98bfa0f89d055817075be6ee93
Powerless <wzxaini9@139.com>
2017-03-12 03:44:56 +0800

    本地备份上传

---
+++ data/conf/db.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'ibtv_fr',
+    'DB_USER' => 'root',
+    'DB_PWD' => 'ibtv_rootmysql',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'i_',
+    //密钥
+    "AUTHCODE" => 'rmEoZ8fgKTjSIQr3mq',
+    //cookies
+    "COOKIE_PREFIX" => 'wGiG3x_',
+);
+?>
